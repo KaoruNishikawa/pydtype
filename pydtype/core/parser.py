4e43a06c08a3d01fb3c948a067186cc6a014445c
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-10-02 13:46:21 +0900

    #2 Fix type annotations

--- pydtype/core/parser.py
+++ pydtype/core/parser.py
@@ -1,16 +1,17 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Union
 
+from .specifier import Specifier
 from ..typing import Shape
 
 
 class Parser(ABC):
     @classmethod
     @abstractmethod
-    def encode(cls, *spec) -> str:
+    def encode(cls, *spec, strategy: str = "exact") -> str:
         ...
 
     @classmethod
     @abstractmethod
-    def decode(cls, spec: str) -> List[Union[str, Tuple[str, int, Shape]]]:
+    def decode(cls, spec: str) -> List[Union[str, Tuple[Specifier, Shape]]]:
         ...
